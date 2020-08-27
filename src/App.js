c86f92848cda77b641f702e2caf8871b2feb65a9
Haris Rozajac <haris.rozajac@fluentworlds.com>
2020-08-27 10:51:07 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Button = (props) => {
+  const handleClick = () => props.onClickFunction(props.increment)
+  return <button onClick={handleClick}>+{props.increment}</button>
 }
 
-export default App;
+const Display = (props) => {
+  return <div>{props.message}</div>
+}
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  const incrementCounter = (incrementValue) =>
+    setCounter(counter + incrementValue)
+  const currentTime = new Date().toLocaleTimeString()
+
+  return (
+    <div className='App'>
+      <h1>React useState() example</h1>
+      <Button onClickFunction={incrementCounter} increment={5} />
+      <Display message={counter} />
+      <h1>Notes</h1>
+      <p>Props can also hold functions. Not just data.</p>
+      <pre>{currentTime}</pre>
+    </div>
+  )
+}
+
+export default App
